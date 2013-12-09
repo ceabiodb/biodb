722307b625d014ddf1f16678848d5e125fa57624
Pierrick Roger Mele <pierrick.rogermele@cea.fr>
2013-12-09 15:01:19 +0100

    Use RUnit.

--- test/test-uniprot.R
+++ test/test-uniprot.R
@@ -1,8 +1,14 @@
 #!/usr/bin/env R --slave -f
+library(RUnit)
 source('../UniProtConn.R', chdir = TRUE)
+
+# Open connexion
 conn <- UniProtConn$new(useragent = "fr.cea.test-uniprot ; pierrick.rogermele@cea.fr")
-entry <- conn$getEntry('Q75MT5')
-entry$getAccession()
-entry$getName()
-entry$getLength()
-entry$getMass()
+
+# Get a real entry
+id <- 'Q75MT5'
+entry <- conn$getEntry(id)
+checkEquals(entry$getAccession(), id)
+checkTrue(entry$getName() != '')
+checkTrue(entry$getLength() > 0)
+checkTrue(entry$getMass() > 0)
