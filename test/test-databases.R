96ddaecabf24b67273544b8ccbc8a591d29a1b09
Pierrick Roger Mele <pierrick.roger@gmail.com>
2016-03-13 23:20:33 +0100

    Reenable massbank test.

--- test/test-databases.R
+++ test/test-databases.R
@@ -22,14 +22,14 @@ online.test.kegg <- function() {
 	test.entries(RBIODB.KEGG, RBIODB.COMPOUND, online = TRUE)
 }
 
-# ############
-# # MASSBANK #
-# ############
-# 
-# offline.test.massbank <- function() {
-# 	test.entries(RBIODB.MASSBANK, RBIODB.SPECTRUM, online = FALSE)
-# }
-# 
-# online.test.massbank <- function() {
-# 	test.entries(RBIODB.MASSBANK, RBIODB.SPECTRUM, online = TRUE)
-# }
+############
+# MASSBANK #
+############
+
+offline.test.massbank <- function() {
+	test.entries(RBIODB.MASSBANK, RBIODB.SPECTRUM, online = FALSE)
+}
+
+online.test.massbank <- function() {
+	test.entries(RBIODB.MASSBANK, RBIODB.SPECTRUM, online = TRUE)
+}
