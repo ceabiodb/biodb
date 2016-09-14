f2835607193872646c30e5803c9d10b0deed3a6f
Pierrick Roger Mele <pierrick.rogermele@cea.fr>
2016-09-14 09:18:55 +0200

    Correct Chemspider entries download and parsing.

--- ChemspiderCompound.R
+++ ChemspiderCompound.R
@@ -18,7 +18,43 @@ if ( ! exists('ChemspiderCompound')) { # Do not load again if already loaded
 
 		compounds <- list()
 
-		print(contents)
+		# Define xpath expressions
+		xpath.expr <- character()
+		xpath.expr[[BIODB.ACCESSION]]    	<- "//CSID"
+		xpath.expr[[BIODB.FORMULA]]      	<- "//MF"
+		xpath.expr[[BIODB.NAME]]         	<- "//CommonName"
+		xpath.expr[[BIODB.AVERAGE.MASS]] 	<- "//AverageMass"
+		xpath.expr[[BIODB.INCHI]]           <- "//InChI"
+		xpath.expr[[BIODB.INCHIKEY]]       	<- "//InChIKey"
+		xpath.expr[[BIODB.SMILES]]          <- "//SMILES"
+
+		for (content in contents) {
+
+			# Create instance
+			compound <- ChemspiderCompound$new()
+
+			if ( ! is.null(content) && ! is.na(content) && content != 'NA') {
+			
+				# Parse XML
+				xml <-  xmlInternalTreeParse(content, asText = TRUE)
+
+				# Test generic xpath expressions
+				for (field in names(xpath.expr)) {
+					v <- xpathSApply(xml, xpath.expr[[field]], xmlValue)
+					if (length(v) > 0)
+						compound$setField(field, v)
+				}
+			}
+
+			compounds <- c(compounds, compound)
+		}
+
+		# Replace elements with no accession id by NULL
+		compounds <- lapply(compounds, function(x) if (is.na(x$getField(BIODB.ACCESSION))) NULL else x)
+
+		# If the input was a single element, then output a single object
+		if (drop && length(contents) == 1)
+			compounds <- compounds[[1]]
 
 		return(compounds)
 	}
