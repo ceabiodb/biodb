dd1be18bd88f68236abaebfdb927adae55fe237b
Pierrick Roger Mele <pierrick.roger@gmail.com>
2017-02-05 17:39:56 +0100

    Writing notes on how use HMDB zip to download.

--- R/HmdbConn.R
+++ R/HmdbConn.R
@@ -34,3 +34,26 @@ HmdbConn$methods( getEntryContent = function(id) {
 HmdbConn$methods( createEntry = function(content, drop = TRUE) {
 	return(createHmdbEntryFromXml(.self$getBiodb(), content, drop = drop))
 })
+
+# Get entry ids {{{1
+################################################################
+
+HmdbConn$methods( getEntryIds = function(max.results = NA_integer_) {
+
+	ids <- NULL
+
+	# TODO Needs first to create BiodbConfig and BiodbCache classes.
+	# TODO Rename HmdbConn to HmdbmetaboliteConn
+
+	# Do we allow database download? This can take some time.
+	if (.self$getBiodb()$getConfig()$enabled(CFG.DBDWNLD)) {
+
+		# Download entries if not already done. --> downloadzip http://www.hmdb.ca/system/downloads/current/hmdb_metabolites.zip
+		# Expand zip, and copy files into cache folder (remove old files first).
+		# TODO How to know it has not already been downloaded?
+		# List files to get all entry IDs
+		# Count files to get number of entries
+	}
+
+	return(ids)
+})
