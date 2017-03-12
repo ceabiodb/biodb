03811c4db71b7bf76fc02501d112db60e448b9e4
Pierrick Roger Mele <pierrick.rogermele@cea.fr>
2017-03-12 18:50:36 +0100

    Use XML attribute parsing in NcbiCcdsEntry.

--- R/NcbiCcdsEntry.R
+++ R/NcbiCcdsEntry.R
@@ -13,44 +13,14 @@ NcbiCcdsEntry <- methods::setRefClass("NcbiCcdsEntry", contains = "HtmlEntry")
 NcbiCcdsEntry$methods( initialize = function(...) {
 
 	callSuper(...)
+
+	.self$addParsingExpression(BIODB.ACCESSION, list(path = "//input[@id='DATA']", attr = "value"))
+	.self$addParsingExpression(BIODB.SEQUENCE, "//b[starts-with(.,'Nucleotide Sequence')]/../tt")
 })
 
-# Parse fields after {{{1
+# Is parsed content correct {{{1
 ################################################################
 
-NcbiCcdsEntry$methods( .parseFieldsAfter = function(parsed.content) {
-
-	if (length(XML::getNodeSet(parsed.content, "//*[starts-with(.,'No results found for CCDS ID ')]")) == 0) {
-		.self$setFieldValue(BIODB.ACCESSION, XML::xpathSApply(parsed.content, "//input[@id='DATA']", XML::xmlGetAttr, "value"))
-		.self$setFieldValue(BIODB.SEQUENCE, XML::xpathSApply(parsed.content, "//b[starts-with(.,'Nucleotide Sequence')]/../tt", XML::xmlValue))
-	}
+NcbiCcdsEntry$methods( .isParsedContentCorrect = function(parsed.content) {
+	return(length(XML::getNodeSet(parsed.content, "//*[starts-with(.,'No results found for CCDS ID ')]")) == 0)
 })
-
-###########
-# FACTORY #
-###########
-
-createNcbiCcdsEntryFromHtml <- function(biodb, contents, drop = TRUE) {
-
-	entries <- list()
-
-	for (html in contents) {
-
-		# Create instance
-		entry <- NcbiCcdsEntry$new(biodb = biodb)
-	
-		# Parse HTML
-		xml <-  XML::htmlTreeParse(html, asText = TRUE, useInternalNodes = TRUE)
-
-		entries <- c(entries, entry)
-	}
-
-	# Replace elements with no accession id by NULL
-	entries <- lapply(entries, function(x) if (is.na(x$getField(BIODB.ACCESSION))) NULL else x)
-
-	# If the input was a single element, then output a single object
-	if (drop && length(contents) == 1)
-		entries <- entries[[1]]
-
-	return(entries)
-}
