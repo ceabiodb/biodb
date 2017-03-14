cea69761be950f1efe7c2e07f14419856d8ab635
Pierrick Roger Mele <pierrick.rogermele@cea.fr>
2017-03-14 13:26:54 +0100

    Parse more fields in KeggCompoundEntry.

--- R/KeggCompoundEntry.R
+++ R/KeggCompoundEntry.R
@@ -14,6 +14,9 @@ KeggCompoundEntry$methods( initialize = function(...) {
 
 	callSuper(...)
 
-	.self$addParsingExpression(BIODB.NAME, "^NAME\\s+([^,;]+)")
 	.self$addParsingExpression(BIODB.ACCESSION, "^ENTRY\\s+(\\S+)\\s+Compound")
+	.self$addParsingExpression(BIODB.NAME, "^NAME\\s+([^,;]+)")
+	.self$addParsingExpression(BIODB.FORMULA, "^FORMULA\\s+(\\S+)$")
+	.self$addParsingExpression(BIODB.MASS, "^EXACT_MASS\\s+(\\S+)$")
+	.self$addParsingExpression(BIODB.MOLECULAR.WEIGHT, "^MOL_WEIGHT\\s+(\\S+)$")
 })
