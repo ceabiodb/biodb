013336781e63a682669468ff15eebb23f3f6815c
Pierrick Roger Mele <pierrick.rogermele@icloud.com>
2017-06-05 16:55:08 +0200

    Send two request for Peakforest: one for LCMS and one for LCMSMS, since we cannot know if an ID refers to LCMS or LCMSMS spectrum.

--- R/PeakforestMassConn.R
+++ R/PeakforestMassConn.R
@@ -24,10 +24,14 @@ PeakforestMassConn$methods( .doGetEntryContentUrl = function(id, concatenate = T
 	if (is.na(.self$getToken()))
 		.self$message(MSG.ERROR, "Peakforest requires a token for this service.")
 
-	if (concatenate)
-		url <- paste(.self$getBaseUrl(), 'spectra/lcmsms/ids/', paste(id, collapse = ','),'?token=', .self$getToken(), sep = '')
-	else
-		url <- paste(.self$getBaseUrl(), 'spectra/lcmsms/ids/', id,'?token=', .self$getToken(), sep = '')
+	if (concatenate) {
+		url <- paste(.self$getBaseUrl(), 'spectra/lcms/ids/', paste(id, collapse = ','),'?token=', .self$getToken(), sep = '')
+		url <- c(url, paste(.self$getBaseUrl(), 'spectra/lcmsms/ids/', paste(id, collapse = ','),'?token=', .self$getToken(), sep = ''))
+	}
+	else {
+		url <- paste(.self$getBaseUrl(), 'spectra/lcms/ids/', id,'?token=', .self$getToken(), sep = '')
+		url <- c(url, paste(.self$getBaseUrl(), 'spectra/lcmsms/ids/', id,'?token=', .self$getToken(), sep = ''))
+	}
 
 	return(url)
 })
