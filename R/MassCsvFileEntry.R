9d7f6d39ba8ee3918a76d8a5764f3573b9c1142b
Pierrick Roger Mele <pierrick.rogermele@icloud.com>
2017-06-13 10:58:37 +0200

    Set precursor MZ field for MassCsvFile.

--- R/MassCsvFileEntry.R
+++ R/MassCsvFileEntry.R
@@ -35,4 +35,25 @@ MassCsvFileEntry$methods( .parseFieldsAfter = function(parsed.content) {
 
 	# Set peaks table in field
 	.self$setFieldValue(BIODB.PEAKS, peaks)
+
+	# Set precursor
+	if (BIODB.PEAK.ATTR %in% colnames(peaks)) {
+		precursors.attr <- .self$getParent()$.precursors
+		precursors <- peaks[[BIODB.PEAK.ATTR]] %in% precursors.attr
+		if (sum(precursors) == 1)
+			.self$setFieldValue(BIODB.MSPRECMZ, peaks[precursors, BIODB.PEAK.MZ])
+		else if (sum(precursors) > 1) {
+			.self$message(MSG.CAUTION, paste("Found more than one precursor inside entry ", .self$getFieldValue(BIODB.ACCESSION, compute = FALSE), ': ', paste(peaks[precursors, BIODB.PEAK.ATTR], collapse = ", "), ". Trying to take the one with highest intensity.", sep = ''))
+			strongest.precursor.mz <- NULL
+			for (int.col in c(BIODB.PEAK.INTENSITY, BIODB.PEAK.RELATIVE.INTENSITY))
+				if (int.col %in% colnames(peaks))
+					strongest.precursor.mz <- peaks[precursors, BIODB.PEAK.MZ][[which(order(peaks[precursors, int.col], decreasing = TRUE) == 1)]]
+			if (is.null(strongest.precursor.mz))
+				.self$message(MSG.CAUTION, 'No intensity information found for choosing the strongest precursor.')
+			else {
+				.self$message(MSG.INFO, paste('Found strongest precursor:', strongest.precursor.mz, '.'))
+				.self$setFieldValue(BIODB.MSPRECMZ, strongest.precursor.mz)
+			}
+		}
+	}
 })
