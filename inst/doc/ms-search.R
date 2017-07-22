0edeaba86c80c69ffcc1b3c8a8017ed53fdecf66
Pierrick Roger Mele <pierrick.rogermele@icloud.com>
2017-07-22 10:47:20 +0200

    Add comments for MS and MSMS search methods in vignette.

--- inst/doc/ms-search.R
+++ inst/doc/ms-search.R
@@ -14,6 +14,9 @@ conn$getMzValues(max.results = 10, ms.level = 2, precursor = TRUE, ms.mode = 'po
 conn$searchMzRange(mz.min = 54, mz.max = 54.1, max.results = 5)
 
 ## ------------------------------------------------------------------------
+conn$searchMzTol(mz = 54, mz.tol = 0.1, mz.tol.unit = 'plain', max.results = 5)
+
+## ------------------------------------------------------------------------
 # Define spectrum to match:
 spectrum <- data.frame(mz = c(100.100, 83.100), rel.int = c(100, 10))
 
