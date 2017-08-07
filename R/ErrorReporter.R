3986f9b8ee4532d8b7a9ec8aee1e2532abfceb8b
Pierrick Roger Mele <pierrick.rogermele@icloud.com>
2017-08-07 11:59:11 +0200

    Document error and warning reporters.

--- R/ErrorReporter.R
+++ R/ErrorReporter.R
@@ -3,6 +3,16 @@
 # CLASS DECLARATION {{{1
 ################################################################
 
+#' A class for logging error messages.
+#'
+#' This class is not meant to be used directly. It is automatically instantiate inside biodb constructor in order to report errors.
+#'
+#' @seealso \code{\link{Biodb}}, \code{\link{BiodbObserver}}.
+#'
+#' @import methods
+#' @include BiodbObserver.R
+#' @export ErrorReporter
+#' @exportClass ErrorReporter
 ErrorReporter <- methods::setRefClass("ErrorReporter", contains = 'BiodbObserver')
 
 # MESSAGE {{{1
