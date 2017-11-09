be3ab401d978599ed4807657173e21156a435f28
Pierrick Roger <pk.roger@icloud.com>
2017-11-09 19:43:45 +0100

    Implement searchCompounds() for Uniprot.

--- R/UniprotConn.R
+++ R/UniprotConn.R
@@ -27,7 +27,7 @@ UniprotConn$methods( initialize = function(...) {
 # Web service query {{{1
 ################################################################
 
-UniprotConn$methods( ws.query = function(columns = NA, format = NA, limit = NA) {
+UniprotConn$methods( ws.query = function(query = '', columns = NA, format = NA, limit = NA) {
 	"Direct query to the database for searching for queries. See http://www.uniprot.org/help/api_queries for details."
 
 	params = list()
@@ -35,20 +35,32 @@ UniprotConn$methods( ws.query = function(columns = NA, format = NA, limit = NA) 
 	# Set URL
 	url <- .self$getBaseUrl()
 
-	# Set query parameter
-	params[['query']] <- ''
-
 	# Set other parameters
-	if ( ! is.na(columns))
-		params[['columns']] <- columns
-	if ( ! is.na(format))
+	params[['query']] <- query
+	if ( ! is.null(columns) && ! is.na(columns))
+		params[['columns']] <- paste(columns, collapse = ',')
+	if ( ! is.null(format) && ! is.na(format))
 		params[['format']] <- format
-	if ( ! is.na(limit))
+	if ( ! is.null(limit) && ! is.na(limit))
 		params[['limit']] <- limit
 
 	result <- .self$.getUrlScheduler()$getUrl(url, params = params)
 
 	return(result)
+})
+
+# Web service query IDs {{{1
+################################################################
+
+UniprotConn$methods( ws.query.ids = function(...) {
+	"Calls ws.query() but only for getting IDs. Returns the IDs as a character vector."
+
+	results <- .self$ws.query(columns = 'id', format = 'tab', ...)
+	readtc <- textConnection(results, "r", local = TRUE)
+	df <- read.table(readtc, sep = "\t", header = TRUE)
+	ids <- as.character(df[[1]])
+
+	return(ids)
 })
 
 # Get entry content {{{1
@@ -74,12 +86,7 @@ UniprotConn$methods( getEntryContent = function(entry.id) {
 
 UniprotConn$methods( getEntryIds = function(max.results = NA_integer_) {
 
-	ids <- NULL
-
-	ids.str <- .self$ws.query(columns = 'id', limit = max.results, format = 'tab')
-	readtc <- textConnection(ids.str, "r", local = TRUE)
-	df <- read.table(readtc, sep = "\t", header = TRUE)
-	ids <- as.character(df[[1]])
+	ids <- .self$ws.query.ids(limit = max.results)
 
 	return(ids)
 })
@@ -92,4 +99,49 @@ UniprotConn$methods( .doGetEntryContentUrl = function(id, concatenate = TRUE) {
 	url <- paste0(.self$getBaseUrl(), id, '.xml')
 
 	return(url)
+})
+
+# Search compound {{{1
+################################################################
+
+UniprotConn$methods( searchCompound = function(name = NULL, mass = NULL, mass.tol = 0.01, mass.tol.unit = 'plain', max.results = NA_integer_) {
+
+	query <- ''
+
+	# Search for name
+	if ( ! is.null(name) && ! is.na(name)) {
+		name.query <- paste('name', paste0('"', name, '"'), sep = ':')
+		mnemonic.query <- paste('mnemonic', paste0('"', name, '"'), sep = ':')
+		query <- paste(name.query, mnemonic.query, sep = ' OR ')
+	}
+
+	# Search for mass
+	if ( ! is.null(mass) && ! is.na(mass)) {
+
+		if (mass.tol.unit == 'ppm') {
+			mass.min <- mass * (1 - mass.tol * 1e-6)
+			mass.max <- mass * (1 + mass.tol * 1e-6)
+		} else {
+			mass.min <- mass - mass.tol
+			mass.max <- mass + mass.tol
+		}
+
+ 		# Uniprot does not accept mass in floating numbers
+		mass.min <- as.integer(mass.min)
+		mass.max <- as.integer(mass.max)
+
+		mass.query <- paste0('mass:[', mass.min, ' TO ', mass.max, ']')
+
+		if (nchar(query) > 0) {
+			query <- paste0('(', query, ')')
+			query <- paste(query, mass.query, sep = ' AND ')
+		}
+		else
+			query <- mass.query
+	}
+
+	# Send query
+	ids <- .self$ws.query.ids(query = query, limit = max.results)
+
+	return(ids)
 })
--- tests/testthat/db-chebi-tests.R
+++ tests/testthat/db-chebi-tests.R
@@ -10,3 +10,13 @@ test.chebi.encoding.issue.in.xml <- function(db) {
 	expect_true(length(entry.ids) > 0)
 	entry <- db$getBiodb()$getFactory()$getEntry(db$getId(), '2571') # +- sign (U+00b1) appears as <c2><b1> if encoding is not set to UTF-8: "<chebiName>(<c2><b1>)-2-Heptanol</chebiName>" instead of "<chebiName>(±)-2-Heptanol</chebiName>"
 }
+
+# Test ChEBI searchCompound for bug 20170926.01
+################################################################
+
+test.chebi.searchCompound.bug.20170926.01 <- function(db) {
+
+	ids <- db$searchCompound(name = "(gamma)Glu-Leu/Ile", mass = 260.1362)
+	expect_true( ! is.null(ids))
+	expect_true(length(ids) > 0)
+}
