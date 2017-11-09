3bcff9ca5d62ba422d52898e779c0a13e6299487
Pierrick Roger <pk.roger@icloud.com>
2017-11-09 11:16:41 +0100

    Test output of Uniprot ws query.

--- tests/testthat/db-uniprot-tests.R
+++ tests/testthat/db-uniprot-tests.R
@@ -8,4 +8,8 @@ test.uniprot.ws.query <- function(db) {
 	expect_true( ! is.null(result))
 	expect_true( ! is.na(result))
 	expect_true(nchar(result) > 0)
+	readtc <- textConnection(result, "r", local = TRUE)
+	df <- read.table(readtc, sep = "\t", header = TRUE)
+	expect_true(colnames(df) == c('Entry'))
+	expect_true(nrow(df) > 0)
 }
