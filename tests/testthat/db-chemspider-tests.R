ae6116f61973d6f8253a06c7e4523b14f1eb3680
Pierrick Roger <pk.roger@icloud.com>
2017-11-14 12:10:23 +0100

    Implement searchCompound in ChemSpider.

--- tests/testthat/db-chemspider-tests.R
+++ tests/testthat/db-chemspider-tests.R
@@ -1,16 +1,27 @@
 # vi: fdm=marker
 
-# Test ChemSpider ws.SearchByMass2
+# Test ChemSpider ws.SearchByMass2 {{{1
 ################################################################
 
 test.chemspider.ws.SearchByMass2 <- function(db) {
 
 	results <- db$ws.SearchByMass2(mass = 96, range = 0.01)
-	print(results)
 	expect_true(nchar(results) > 0)
 
 	ids <- db$ws.SearchByMass2.ids(mass = 96, range = 0.01)
-	print(ids)
+	expect_true(is.character(ids))
+	expect_true(length(ids) > 0)
+}
+
+# Test ChemSpider ws.SimpleSearch {{{1
+################################################################
+
+test.chemspider.ws.SimpleSearch <- function(db) {
+
+	results <- db$ws.SimpleSearch(query = 'benzene')
+	expect_true(nchar(results) > 0)
+
+	ids <- db$ws.SimpleSearch.ids(query = 'benzene')
 	expect_true(is.character(ids))
 	expect_true(length(ids) > 0)
 }
@@ -19,6 +30,8 @@ test.chemspider.ws.SearchByMass2 <- function(db) {
 ################################################################
 
 run.chemspider.tests <- function(db, mode) {
-	if (mode %in% c(MODE.ONLINE, MODE.QUICK.ONLINE))
+	if (mode %in% c(MODE.ONLINE, MODE.QUICK.ONLINE)) {
 		run.db.test('ChemSpider web service search by mass works fine.', 'test.chemspider.ws.SearchByMass2', db)
+		run.db.test('ChemSpider web service simple search works fine.', 'test.chemspider.ws.SimpleSearch', db)
+	}
 }
