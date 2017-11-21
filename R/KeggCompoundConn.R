5e3d26feb879b87cd2e69427a0bdf5469cc3b29f
Pierrick Roger <pk.roger@icloud.com>
2017-11-21 15:27:42 +0100

    Start implementing searchCompound() for KEGG Compound.

--- R/KeggCompoundConn.R
+++ R/KeggCompoundConn.R
@@ -3,8 +3,21 @@
 # Class declaration {{{1
 ################################################################
 
+#' The connector class to KEGG Compound database.
+#'
+#' This is a concrete connector class. It must never be instantiated directly, but instead be instantiated through the factory \code{\link{BiodbFactory}}. Only specific methods are described here. See super classes for the description of inherited methods.
+#'
+#' @param
+#'
+#' @seealso \code{\link{BiodbFactory}}, \code{\link{RemotedbConn}}, \code{\link{CompounddbConn}}.
+#'
+#' @examples
+#' # TODO
+#'
 #' @include KeggConn.R
 #' @include CompounddbConn.R
+#' @export KeggCompoundConn
+#' @exportClass KeggCompoundConn
 KeggCompoundConn <- methods::setRefClass("KeggCompoundConn", contains = c("KeggConn", "CompounddbConn"))
 
 # Constructor {{{1
@@ -14,3 +27,16 @@ KeggCompoundConn$methods( initialize = function(...) {
 	callSuper(db.name = 'compound', db.abbrev = 'cpd', ...)
 })
 
+# Web service find exact mass {{{1
+################################################################
+
+KeggCompoundConn$methods( ws.find.exact.mass = function(mass) {
+
+	url <- paste(.self$getBaseUrl(), 'find/', .self$.db.name, '/')
+})
+
+# Search compound {{{1
+################################################################
+
+KeggCompoundConn$methods( searchCompound = function(name = NULL, mass = NULL, mass.tol = 0.01, mass.tol.unit = 'plain', max.results = NA_integer_) {
+})
