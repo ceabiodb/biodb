c55a224cbfaff1489667322ca6843d67f4dee8f9
Pierrick Roger <pk.roger@icloud.com>
2017-12-14 12:10:05 +0100

    Improve massbank examples.
    Correct test of msmsSearch, and find better spectrum values for
    msmsSearch example and vignette.

--- examples/massbank.jp-msms-search.R
+++ examples/massbank.jp-msms-search.R
@@ -4,7 +4,8 @@ mybiodb <- biodb::Biodb$new()
 # Get connector to Massbank
 massbank <- mybiodb$getFactory()$getConn('massbank.jp')
 
-spectrum <- data.frame(mz = c(64), rel.int = c(100))
+# Create spectrum to search for
+spectrum <- data.frame(mz = c(156.0124, 252.0446), rel.int = c(999, 158))
 
-# Search for MS spectra
-massbank$msmsSearch(spectrum, precursor.mz = 100, mz.tol = 0.3)
+# Search for matching MS spectra
+massbank$msmsSearch(spectrum, precursor.mz = 252.0448, mz.tol = 0.1, mz.tol.unit = 'plain', ms.mode = 'neg')
