59363144db2a426aa10fb248ad0961561d5c5376
Pierrick Roger <pk.roger@icloud.com>
2018-02-02 14:52:56 +0100

    Correct typo.

--- R/MassdbConn.R
+++ R/MassdbConn.R
@@ -148,7 +148,7 @@ MassdbConn$methods ( searchMsPeaks = function(mzs, mz.tol, mz.tol.unit = BIODB.M
 		ids <- .self$searchMzTol(mz, mz.tol = mz.tol, mz.tol.unit = mz.tol.unit, min.rel.int = min.rel.int, ms.mode = ms.mode, max.results = max.results, ms.level = 1)
 
 		# Get entries
-		.self$message('debug', 'Getting entries from spetrac IDs.')
+		.self$message('debug', 'Getting entries from spectra IDs.')
 		entries <- .self$getBiodb()$getFactory()$getEntry(.self$getId(), ids, drop = FALSE)
 
 		# Convert to data frame
