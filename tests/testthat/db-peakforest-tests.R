e6c7106a4dde2a7ae8252d88ba06d40b2b6f49e0
Pierrick Roger <pk.roger@icloud.com>
2018-02-15 17:35:23 +0100

    Factorize web services for Peakforest.
    Implement all.count and all.ids web services.

--- tests/testthat/db-peakforest-tests.R
+++ tests/testthat/db-peakforest-tests.R
@@ -23,11 +23,41 @@ test.peakforest.compound.ws.search <- function(db) {
 	expect_length(results, 2)
 }
 
+# Test web service "all.count" {{{1
+################################################################
+
+test.peakforest.compound.ws.all.count <- function(db) {
+	results <- db$ws.all.count()
+	expect_is(results, 'character')
+	expect_length(results, 1)
+	results <- db$ws.all.count(biodb.parse = TRUE)
+	expect_is(results, 'integer')
+	expect_length(results, 1)
+	expect_gt(results, 0)
+}
+
+# Test web service "all.ids" {{{1
+################################################################
+
+test.peakforest.compound.ws.all.ids <- function(db) {
+	results <- db$ws.all.ids()
+	expect_is(results, 'character')
+	expect_length(results, 1)
+	results <- db$ws.all.ids(biodb.parse = TRUE)
+	expect_is(results, 'integer')
+	expect_true(length(results) > 0)
+	results <- db$ws.all.ids(biodb.ids = TRUE)
+	expect_is(results, 'character')
+	expect_true(length(results) > 0)
+}
+
 # Run PeakForest Compound tests {{{1
 ################################################################
 
 run.peakforest.compound.tests <- function(db, mode) {
 	if (mode %in% c(MODE.ONLINE, MODE.QUICK.ONLINE)) {
 		run.db.test.that('Web service "search" works.', 'test.peakforest.compound.ws.search', db)
+		run.db.test.that('Web service "all.count" works.', 'test.peakforest.compound.ws.all.count', db)
+		run.db.test.that('Web service "all.ids" works.', 'test.peakforest.compound.ws.all.ids', db)
 	}
 }
