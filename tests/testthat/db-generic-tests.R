0d8bc3ce2bce77a8ad1f2eeb64c26211d27ba617
Pierrick Roger <pk.roger@icloud.com>
2018-03-03 18:54:39 +0100

    Add some tests.

--- tests/testthat/db-generic-tests.R
+++ tests/testthat/db-generic-tests.R
@@ -250,13 +250,15 @@ test.entry.image.url.download <- function(db) {
 ################################################################
 
 test.nothing <- function(db) {
+	biodb <- db$getBiodb()
+	db.name <- db$getId()
 }
 
 run.db.generic.tests <- function(db, mode) {
 
 	set.test.context(db$getBiodb(), paste("Running generic tests on database", db$getId(), "in", mode, "mode"))
 
-	run.db.test.that("Wrong entry gives NULL", 'test.nothing', db)
+	run.db.test.that("Does nothing", 'test.nothing', db)
 	run.db.test.that("Wrong entry gives NULL", 'test.wrong.entry', db)
 	run.db.test.that("One wrong entry does not block the retrieval of good ones", 'test.wrong.entry.among.good.ones', db)
 	run.db.test.that("Entry fields have a correct value", 'test.entry.fields', db)
