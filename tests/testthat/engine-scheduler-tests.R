d635e9591758038ca0835341714f8157b8a6639b
Pierrick Roger <pk.roger@icloud.com>
2018-11-14 23:50:40 +0100

    Note on which test to write.

--- tests/testthat/engine-scheduler-tests.R
+++ tests/testthat/engine-scheduler-tests.R
@@ -6,4 +6,9 @@
 run.scheduler.tests <- function(biodb, obs) {
 
 	set.test.context(biodb, "Test request scheduler")
+
+	# TODO do some tests offline:
+	# 1. Test that ChEBI connector create the right rule (Write some private methods inside scheduler to get info about rules).
+	# 2. Test that frequency are updated correctly if another connector is created with different n and t (or same n and t).
+	# 3. Test waiting time of scheduler. How?
 }
