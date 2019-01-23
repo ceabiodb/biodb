d787e21b6ea48966ecf20252e09cfbebfd202227
Pierrick Roger <pk.roger@icloud.com>
2019-01-23 19:22:45 +0100

    Accept a data frame as input in searchMsPeaks(). (#287)
    * Accept a data frame as input in searchMsPeaks().
    
    Now we can input a full data frame containing M/Z and RT columns, but
    also other unused columns into searchMsPeaks().
    The unused columns are output in the results data frame if
    insert.input.values is set to TRUE.
    
    * Sort columns in results data frame.
    
    * Authorize no chrom cols in RT match.
    
    * Create all connectors when running database tests.
    
    * Add compulsory match.rt param for running RT match.

--- tests/testthat/db-mass.csv.file-tests.R
+++ tests/testthat/db-mass.csv.file-tests.R
@@ -241,6 +241,27 @@ test.mass.csv.file.entry.peaks.table.col.names <- function(biodb) {
 	expect_true(all(vapply(names(peaks), function(field) biodb$getEntryFields()$get(field)$getName() == field, FUN.VALUE = FALSE)))
 }
 
+# Test column sorting in searchMsPeaks() {{{1
+################################################################
+
+test.mass.csv.file.searchMsPeaks.column.sorting <- function(biodb) {
+
+	# Define db data frame
+	db.df <- rbind(data.frame(accession = 'C1', ms.mode = 'POS', peak.mztheo = 112.07569, peak.comp = 'P9Z6W410 O', peak.attr = '[(M+H)-(H2O)-(NH3)]+', formula = "J114L6M62O2", molecular.mass = 146.10553, name = 'Blablaine', inchi = 'InChI=1S/C7H8N4O3/c1-10-4-3(8-6(10)13)5(12)11(2)7(14)9-4/h1-2H3,(H,8,13)(H,9,14)', inchikey = 'NA'),
+	               data.frame(accession = 'C2', ms.mode = 'POS', peak.mztheo = 208.8274, peak.comp = 'Y2Z6W410 O', peak.attr = '[(M+H)-(H2O)]+', formula = "T114L6M62O2", molecular.mass = 146.10553, name = 'Sloopaine', inchi = 'InChI=1S/C7H8N4O3/c1-10-4-3(8-6(10)13)5(12)11(2)7(14)9-4/h1-2H3,(H,8,13)(H,9,14)', inchikey = 'FRYVQXCDSBCDAU-AKDOOQIZSA-N'),
+	               stringsAsFactors = FALSE)
+
+	# Create connector
+	conn <- biodb$getFactory()$createConn('mass.csv.file')
+	conn$setDb(db.df)
+
+	# Search
+	results <- conn$searchMsPeaks(db.df[['peak.mztheo']], mz.tol = 10, insert.input.values = FALSE)
+	expect_is(results, 'data.frame')
+	expected.cols <- sort(c(colnames(db.df), 'peak.mz', 'mass.csv.file.id'))
+	expect_identical(expected.cols, colnames(results))
+}
+
 # Test M/Z matching limits {{{1
 ################################################################
 
@@ -270,16 +291,16 @@ test.mass.csv.file.mz.matching.limits <- function(biodb) {
 		expect_is(results2, 'data.frame')
 		results3 <- conn$searchMsPeaks(mz.inf, mz.shift = mz.shift, mz.tol = mz.tol, mz.tol.unit = mz.tol.unit, ms.mode = 'pos')
 		expect_is(results3, 'data.frame')
-		results4 <- conn$searchMsPeaks(mz.inf - 1e-6, mz.shift = mz.shift, mz.tol = mz.tol, mz.tol.unit = mz.tol.unit, ms.mode = 'pos', insert.mz.rt.values = FALSE)
+		results4 <- conn$searchMsPeaks(mz.inf - 1e-6, mz.shift = mz.shift, mz.tol = mz.tol, mz.tol.unit = mz.tol.unit, ms.mode = 'pos', insert.input.values = FALSE)
 		expect_null(results4)
-		results4.1 <- conn$searchMsPeaks(mz.inf - 1e-6, mz.shift = mz.shift, mz.tol = mz.tol, mz.tol.unit = mz.tol.unit, ms.mode = 'pos', insert.mz.rt.values = TRUE)
+		results4.1 <- conn$searchMsPeaks(mz.inf - 1e-6, mz.shift = mz.shift, mz.tol = mz.tol, mz.tol.unit = mz.tol.unit, ms.mode = 'pos', insert.input.values = TRUE)
 		expect_is(results4.1, 'data.frame')
 		expect_identical(colnames(results4.1), 'mz')
 		results5 <- conn$searchMsPeaks(mz.sup, mz.shift = mz.shift, mz.tol = mz.tol, mz.tol.unit = mz.tol.unit, ms.mode = 'pos')
 		expect_is(results5, 'data.frame')
-		results6 <- conn$searchMsPeaks(mz.sup + 1e-6, mz.shift = mz.shift, mz.tol = mz.tol, mz.tol.unit = mz.tol.unit, ms.mode = 'pos', insert.mz.rt.values = FALSE)
+		results6 <- conn$searchMsPeaks(mz.sup + 1e-6, mz.shift = mz.shift, mz.tol = mz.tol, mz.tol.unit = mz.tol.unit, ms.mode = 'pos', insert.input.values = FALSE)
 		expect_null(results6)
-		results6.1 <- conn$searchMsPeaks(mz.sup + 1e-6, mz.shift = mz.shift, mz.tol = mz.tol, mz.tol.unit = mz.tol.unit, ms.mode = 'pos', insert.mz.rt.values = TRUE)
+		results6.1 <- conn$searchMsPeaks(mz.sup + 1e-6, mz.shift = mz.shift, mz.tol = mz.tol, mz.tol.unit = mz.tol.unit, ms.mode = 'pos', insert.input.values = TRUE)
 		expect_is(results6.1, 'data.frame')
 		expect_identical(colnames(results6.1), 'mz')
 	}
@@ -320,16 +341,16 @@ test.mass.csv.file.rt.matching.limits <- function(biodb) {
 	expect_is(results2, 'data.frame')
 	results3 <- conn$searchMsPeaks(mz, mz.shift = mz.shift, mz.tol = mz.tol, mz.tol.unit = mz.tol.unit, ms.mode = 'pos', chrom.col.ids = col.id, rt = rt.inf, rt.unit = rt.unit, rt.tol = x, rt.tol.exp = y)
 	expect_is(results3, 'data.frame')
-	results4 <- conn$searchMsPeaks(mz, mz.shift = mz.shift, mz.tol = mz.tol, mz.tol.unit = mz.tol.unit, ms.mode = 'pos', chrom.col.ids = col.id, rt = rt.inf - 1e-6, rt.unit = rt.unit, rt.tol = x, rt.tol.exp = y, insert.mz.rt.values = FALSE)
+	results4 <- conn$searchMsPeaks(mz, mz.shift = mz.shift, mz.tol = mz.tol, mz.tol.unit = mz.tol.unit, ms.mode = 'pos', chrom.col.ids = col.id, rt = rt.inf - 1e-6, rt.unit = rt.unit, rt.tol = x, rt.tol.exp = y, insert.input.values = FALSE)
 	expect_null(results4)
-	results4.1 <- conn$searchMsPeaks(mz, mz.shift = mz.shift, mz.tol = mz.tol, mz.tol.unit = mz.tol.unit, ms.mode = 'pos', chrom.col.ids = col.id, rt = rt.inf - 1e-6, rt.unit = rt.unit, rt.tol = x, rt.tol.exp = y, insert.mz.rt.values = TRUE)
+	results4.1 <- conn$searchMsPeaks(mz, mz.shift = mz.shift, mz.tol = mz.tol, mz.tol.unit = mz.tol.unit, ms.mode = 'pos', chrom.col.ids = col.id, rt = rt.inf - 1e-6, rt.unit = rt.unit, rt.tol = x, rt.tol.exp = y, insert.input.values = TRUE)
 	expect_is(results4.1, 'data.frame')
 	expect_identical(colnames(results4.1), c('mz', 'rt'))
 	results5 <- conn$searchMsPeaks(mz, mz.shift = mz.shift, mz.tol = mz.tol, mz.tol.unit = mz.tol.unit, ms.mode = 'pos', chrom.col.ids = col.id, rt = rt.sup, rt.unit = rt.unit, rt.tol = x, rt.tol.exp = y)
 	expect_is(results5, 'data.frame')
-	results6 <- conn$searchMsPeaks(mz, mz.shift = mz.shift, mz.tol = mz.tol, mz.tol.unit = mz.tol.unit, ms.mode = 'pos', chrom.col.ids = col.id, rt = rt.sup + 1e-6, rt.unit = rt.unit, rt.tol = x, rt.tol.exp = y, insert.mz.rt.values = FALSE)
+	results6 <- conn$searchMsPeaks(mz, mz.shift = mz.shift, mz.tol = mz.tol, mz.tol.unit = mz.tol.unit, ms.mode = 'pos', chrom.col.ids = col.id, rt = rt.sup + 1e-6, rt.unit = rt.unit, rt.tol = x, rt.tol.exp = y, insert.input.values = FALSE)
 	expect_null(results6)
-	results6.1 <- conn$searchMsPeaks(mz, mz.shift = mz.shift, mz.tol = mz.tol, mz.tol.unit = mz.tol.unit, ms.mode = 'pos', chrom.col.ids = col.id, rt = rt.sup + 1e-6, rt.unit = rt.unit, rt.tol = x, rt.tol.exp = y, insert.mz.rt.values = TRUE)
+	results6.1 <- conn$searchMsPeaks(mz, mz.shift = mz.shift, mz.tol = mz.tol, mz.tol.unit = mz.tol.unit, ms.mode = 'pos', chrom.col.ids = col.id, rt = rt.sup + 1e-6, rt.unit = rt.unit, rt.tol = x, rt.tol.exp = y, insert.input.values = TRUE)
 	expect_is(results6.1, 'data.frame')
 	expect_identical(colnames(results6.1), c('mz', 'rt'))
 }
@@ -520,4 +541,5 @@ run.mass.csv.file.tests <- function(db, mode) {
 	run.test.that.on.biodb('Database writing works.', 'test.mass.csv.file.writing', biodb)
 	run.test.that.on.biodb('Adding a new entry to the database works.', 'test.mass.csv.file.add.new.entry', biodb)
 	run.test.that.on.biodb('Two different databases do not use the same cache files.', 'test.mass.csv.file.cache.confusion', biodb)
+	run.test.that.on.biodb('Sorting of columns of result frame works well in searchMsPeaks().', 'test.mass.csv.file.searchMsPeaks.column.sorting', biodb)
 }
