3124c0fe508970bfd5fe93863840e0836c58228d
Pierrick Roger <pk.roger@icloud.com>
2019-05-23 14:30:32 +0200

    Move all database definitions to JSON file.

--- R/BiodbDbsInfo.R
+++ R/BiodbDbsInfo.R
@@ -121,29 +121,6 @@ BiodbDbsInfo$methods( .initDbsInfo = function() {
 	# Load JSON databases information file
 	dbf <- .self$getBiodb()$getConfig()$get('dbinfo.file')
 	.self$loadInfoFile(dbf)
-
-#	.self$.define('chebi',                  properties = list(name = 'ChEBI',  scheduler.n = 3, entry.content.type = 'xml', entry.content.encoding = 'UTF-8', urls = c(base.url = 'https://www.ebi.ac.uk/chebi/', ws.url = 'https://www.ebi.ac.uk/webservices/chebi/2.0/'), xml.ns = c(chebi = "https://www.ebi.ac.uk/webservices/chebi", xsd = "http://www.w3.org/2001/XMLSchema")))
-	.self$.define('chemspider',             properties = list(name = 'ChemSpider',  scheduler.n = 3, entry.content.type = 'json', urls = c(base.url = "http://www.chemspider.com/", ws.url = "https://api.rsc.org/compounds/v1/")))
-	.self$.define('expasy.enzyme',          properties = list(name = 'ExPASy ENZYME',  scheduler.n = 3, entry.content.type = 'txt', urls = c(base.url = "https://enzyme.expasy.org/")))
-	.self$.define('hmdb.metabolites',       properties = list(name = 'HMDB Metabolites',  scheduler.n = 3, entry.content.type = 'xml', urls = c(base.url = "http://www.hmdb.ca/")))
-	.self$.define('kegg.compound',          properties = list(name = 'KEGG Compound',  scheduler.n = 3, entry.content.type = 'txt', urls = c(base.url = 'https://www.kegg.jp/', ws.url = 'http://rest.kegg.jp/', entry.page.url = 'https://www.genome.jp/dbget-bin')))
-	.self$.define('kegg.enzyme',            properties = list(name = 'KEGG Enzyme',  scheduler.n = 3, entry.content.type = 'txt', urls = c(base.url = 'https://www.kegg.jp/', ws.url = 'http://rest.kegg.jp/', entry.page.url = 'https://www.genome.jp/dbget-bin')))
-	.self$.define('kegg.genes',            properties = list(name = 'KEGG Genes',  scheduler.n = 3, entry.content.type = 'txt', urls = c(base.url = 'https://www.kegg.jp/', ws.url = 'http://rest.kegg.jp/', entry.page.url = 'https://www.genome.jp/dbget-bin')))
-	.self$.define('kegg.module',            properties = list(name = 'KEGG Module',  scheduler.n = 3, entry.content.type = 'txt', urls = c(base.url = 'https://www.kegg.jp/', ws.url = 'http://rest.kegg.jp/', entry.page.url = 'https://www.genome.jp/dbget-bin')))
-	.self$.define('kegg.pathway',           properties = list(name = 'KEGG Pathway',  scheduler.n = 3, entry.content.type = 'txt', urls = c(base.url = 'https://www.kegg.jp/', ws.url = 'http://rest.kegg.jp/', entry.page.url = 'https://www.genome.jp/dbget-bin')))
-	.self$.define('kegg.reaction',          properties = list(name = 'KEGG Reaction',  scheduler.n = 3, entry.content.type = 'txt', urls = c(base.url = 'https://www.kegg.jp/', ws.url = 'http://rest.kegg.jp/', entry.page.url = 'https://www.genome.jp/dbget-bin')))
-	.self$.define('lipidmaps.structure',    properties = list(name = 'LIPID MAPS Structure',  scheduler.n = 1, scheduler.t = 20, entry.content.type = 'csv', urls = c(base.url = 'http://www.lipidmaps.org/data/'))) # About access frequency, see http://www.lipidmaps.org/data/structure/programmaticaccess.html
-	.self$.define('mass.csv.file',          properties = list(name = 'Mass CSV File',  entry.content.type = 'tsv', urls = character()))
-	.self$.define('mass.sqlite',            properties = list(name = 'Mass SQLite',  entry.content.type = 'list', urls = character()))
-	.self$.define('massbank',               properties = list(name = 'MassBank',  scheduler.n = 3, entry.content.type = 'txt', urls = c(base.url = 'https://massbank.eu/', db.tar.url = 'https://github.com/MassBank/MassBank-data/archive/master.tar.gz', prefixes.file.url = 'https://raw.githubusercontent.com/MassBank/MassBank-data/master/List_of_Contributors_Prefixes_and_Projects.md')))
-	.self$.define('mirbase.mature',         properties = list(name = 'miRBase Mature',  scheduler.n = 3, entry.content.type = 'txt', urls = c(base.url = "http://www.mirbase.org/", ftp.url = "ftp://mirbase.org/pub/mirbase/CURRENT/")))
-	.self$.define('ncbi.ccds',              properties = list(name = 'NCBI CCDS',  scheduler.n = 3, entry.content.type = 'html', urls = c(base.url = 'https://www.ncbi.nlm.nih.gov/CCDS/', ws.url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils')))
-	.self$.define('ncbi.gene',              properties = list(name = 'NCBI Gene',  scheduler.n = 3, entry.content.type = 'xml', urls = c(base.url = 'https://www.ncbi.nlm.nih.gov/', ws.url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/'))) # About access frequency, see https://www.ncbi.nlm.nih.gov/books/NBK25497/
-	.self$.define('ncbi.pubchem.comp',      properties = list(name = 'PubChem Compound',  scheduler.n = 5, entry.content.type = 'xml', urls = c(base.url = 'https://pubchem.ncbi.nlm.nih.gov/', ws.url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/', ws2.url = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/'))) # About access frequency, see https://pubchem.ncbi.nlm.nih.gov/pug_rest/PUG_REST.html
-	.self$.define('ncbi.pubchem.subst',     properties = list(name = 'PubChem Substance',  scheduler.n = 5, entry.content.type = 'xml', urls = c(base.url = 'https://pubchem.ncbi.nlm.nih.gov/', ws.url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/', ws2.url = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/'))) # About access frequency, see https://pubchem.ncbi.nlm.nih.gov/pug_rest/PUG_REST.html
-	.self$.define('peakforest.mass',        properties = list(name = 'PeakForest Mass',  scheduler.n = 3, entry.content.type = 'json', urls = c(base.url = 'https://metabohub.peakforest.org/webapp/home', ws.url = 'https://metabohub.peakforest.org/rest/')))
-	.self$.define('peakforest.compound',    properties = list(name = 'PeakForest Compound',  scheduler.n = 3, entry.content.type = 'json', urls = c(base.url = 'https://metabohub.peakforest.org/webapp/home', ws.url = 'https://metabohub.peakforest.org/rest/')))
-	.self$.define('uniprot',                properties = list(name = 'UniProt',  entry.content.type = 'xml', urls = c(base.url = 'https://www.uniprot.org/uniprot/'), xml.ns = c(uniprot = "http://uniprot.org/uniprot")))
 })
 
 # Define {{{2
