27c4dfa079f21d06b61b1290896c78ddd6b0da71
Pierrick Roger <pk.roger@icloud.com>
2019-07-02 15:12:42 +0200

    Resize KeggGenes lines.

--- R/KeggGenesConn.R
+++ R/KeggGenesConn.R
@@ -1,30 +1,40 @@
 # vi: fdm=marker ts=4 et cc=80 tw=80
 
-# Class declaration {{{1
+# KeggGenesConn {{{1
 ################################################################################
 
 #' The connector class to KEGG Pathway database.
 #'
-#' This is a concrete connector class. It must never be instantiated directly, but instead be instantiated through the factory \code{\link{BiodbFactory}}. Only specific methods are described here. See super classes for the description of inherited methods.
+#' This is a concrete connector class. It must never be instantiated directly,
+#' but instead be instantiated through the factory \code{\link{BiodbFactory}}.
+#' Only specific methods are described here. See super classes for the
+#' description of inherited methods.
 #'
 #' @seealso \code{\link{BiodbFactory}}, \code{\link{KeggConn}}.
 #'
 #' @include KeggConn.R
 #' @export KeggGenesConn
 #' @exportClass KeggGenesConn
-KeggGenesConn <- methods::setRefClass("KeggGenesConn", contains=c("KeggConn"))
+KeggGenesConn <- methods::setRefClass("KeggGenesConn",
+    contains=c("KeggConn"),
 
-# Initialize {{{1
+# Public methods {{{2
 ################################################################################
 
-KeggGenesConn$methods( initialize=function(...) {
+methods=list(
+
+# Initialize {{{3
+################################################################################
+
+initialize=function(...) {
     callSuper(db.name='genes', ...)
-})
+},
 
-# Get entry image url {{{1
+# Get entry image url {{{3
 ################################################################################
 
-KeggGenesConn$methods( getEntryImageUrl=function(id) {
+getEntryImageUrl=function(id) {
     return(rep(NA_character_, length(id)))
-})
+}
 
+))
--- R/KeggGenesEntry.R
+++ R/KeggGenesEntry.R
@@ -1,46 +1,63 @@
 # vi: fdm=marker ts=4 et cc=80 tw=80
 
+# KeggGenesEntry {{{1
+################################################################################
+
 #' @include KeggEntry.R
+KeggGenesEntry <- methods::setRefClass("KeggGenesEntry",
+    contains='KeggEntry',
 
-# Class declaration {{{1
+# Public methods {{{2
 ################################################################################
 
-KeggGenesEntry <- methods::setRefClass("KeggGenesEntry", contains='KeggEntry')
+methods=list(
 
-# Initialize {{{1
+# Initialize {{{3
 ################################################################################
 
-KeggGenesEntry$methods( initialize=function(...) {
+initialize=function(...) {
 
     callSuper(...)
-})
+},
 
-# Parse fields step 2 {{{1
+# Private methods {{{2
 ################################################################################
 
-KeggGenesEntry$methods( .parseFieldsStep2=function(parsed.content) {
+# Parse fields step 2 {{{3
+################################################################################
+
+.parseFieldsStep2=function(parsed.content) {
 
     # Name
-    .self$.parseMultilinesField(field='name', tag='NAME', parsed.content=parsed.content, strip.chars=' ;', split.char=',')
+    .self$.parseMultilinesField(field='name', tag='NAME',
+                                parsed.content=parsed.content,
+                                strip.chars=' ;', split.char=',')
 
     # Adjust accession with organism code
-    if (.self$hasField('kegg.organism.code'))
-        .self$setFieldValue('accession', paste(.self$getFieldValue('kegg.organism.code'), .self$getFieldValue('accession'), sep=':'))
+    if (.self$hasField('kegg.organism.code')) {
+        org <- .self$getFieldValue('kegg.organism.code')
+        acc <- .self$getFieldValue('accession')
+        .self$setFieldValue('accession', paste(org, acc, sep=':'))
+    }
 
     # Other KEGG IDs
     .self$.parseModuleIds(parsed.content)
     .self$.parsePathwayIds(parsed.content=parsed.content)
 
     # Parse Uniprot IDs
-    if (.self$hasField('uniprot.id'))
-        .self$setFieldValue('uniprot.id', strsplit(.self$getFieldValue('uniprot.id'), ' +', perl=TRUE)[[1]])
+    if (.self$hasField('uniprot.id')) {
+        ids <- strsplit(.self$getFieldValue('uniprot.id'), ' +', perl=TRUE)[[1]]
+        .self$setFieldValue('uniprot.id', ids)
+    }
 
     # AA SEQ
     lines <- .self$.getTagLines(tag='AASEQ', parsed.content=parsed.content)
     seq.length <- as.integer(lines[[1]])
     sequence <- paste(lines[2:length(lines)], collapse='')
     if (seq.length != nchar(sequence))
-        .self$message('CAUTION', paste('Length of AA sequence (', nchar(sequence), ') is different from the stated length (', seq.length, '). In entry ', .self$getFieldValue('accession'), '.'))
+        .self$caution('Length of AA sequence (', nchar(sequence),
+                      ') is different from the stated length (', seq.length,
+                      '). In entry ', .self$getFieldValue('accession'), '.')
     .self$setFieldValue('aa.seq', sequence)
     .self$setFieldValue('aa.seq.length', seq.length)
 
@@ -49,7 +66,11 @@ KeggGenesEntry$methods( .parseFieldsStep2=function(parsed.content) {
     seq.length <- as.integer(lines[[1]])
     sequence <- paste(lines[2:length(lines)], collapse='')
     if (seq.length != nchar(sequence))
-        .self$message('CAUTION', paste('Length of NT sequence (', nchar(sequence), ') is different from the stated length (', seq.length, '). In entry ', .self$getFieldValue('accession'), '.'))
+        .self$caution('Length of NT sequence (', nchar(sequence),
+                      ') is different from the stated length (', seq.length,
+                      '). In entry ', .self$getFieldValue('accession'), '.')
     .self$setFieldValue('nt.seq', sequence)
     .self$setFieldValue('nt.seq.length', seq.length)
-})
+}
+
+))
