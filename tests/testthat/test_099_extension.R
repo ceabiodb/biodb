5fe6e2a09a61febbd71e44474768ac151948ec12
Pierrick Roger <pk.roger@icloud.com>
2021-04-28 16:20:55 +0200

    Move ext pkg make test in long tests.

--- tests/testthat/test_099_extension.R
+++ tests/testthat/test_099_extension.R
@@ -140,50 +140,6 @@ test_newExtPkgSkeletonGeneration <- function() {
     }
 }
 
-test_runningMakeOnNewExtPkg <- function() {
-    
-    # We run make on just one pkg, since it is quite long
-
-    connType <- 'compound'
-    entryType <- 'xml'
-    name <- c('foo2', 'local', 'rcpp')
-    dbName <- paste(name, collapse='.')
-    clsPrefix <- biodb:::connNameToClassPrefix(dbName)
-    pkgName <- paste0('biodb', clsPrefix)
-
-    # Folder of the new package
-    pkgDir <- file.path(getwd(), 'output', pkgName)
-    if (dir.exists(pkgDir))
-        unlink(pkgDir, recursive=TRUE)
-    if ( ! dir.exists(dirname(pkgDir)))
-        dir.create(dirname(pkgDir))
-
-    # Create a new extension package skeleton
-    biodb::ExtPackage$new(path=pkgDir, dbName=dbName,
-                          dbTitle='FOO database',
-                          connType=connType, entryType=entryType,
-                          remote=FALSE,
-                          editable=TRUE, writable=TRUE,
-                          makefile=TRUE, rcpp=TRUE)$generate()
-    
-    # Check targets
-    biodb::logDebug('Package directory is "%s".', pkgDir)
-    makeLog <- paste(pkgDir, 'make', 'doc', 'log', sep='.')
-    biodb::logDebug('Make log file is "%s".', makeLog)
-    system2('make', c('-C', pkgDir, 'doc'), stdout=makeLog, stderr=makeLog)
-    testthat::expect_true(file.exists(file.path(pkgDir, 'NAMESPACE'),
-                                      info=paste0('Running make doc inside "',
-                                                  pkgDir,
-                                                  '" failed. See output in "',
-                                                  makeLog, '".')))
-    makeLog <- paste(pkgDir, 'make', 'log', sep='.')
-    biodb::logDebug('Make log file is "%s".', makeLog)
-    system2('make', c('-C', pkgDir), stdout=makeLog, stderr=makeLog)
-    makeLog <- paste(pkgDir, 'make', 'test', 'log', sep='.')
-    biodb::logDebug('Make log file is "%s".', makeLog)
-    system2('make', c('-C', pkgDir, 'test'), stdout=makeLog, stderr=makeLog)
-}
-
 test_upgradeExtPkg <- function() {
     
     for (cfg in list(list(connType='plain', remote=TRUE, entryType='plain',
@@ -294,9 +250,6 @@ test_getReposName <- function() {
     testthat::expect_equal(paste0('pkrog/', repos), name)
 }
 
-# Main
-################################################################################
-
 # Instantiate Biodb
 biodb <- biodb::createBiodbTestInstance()
 
@@ -312,9 +265,6 @@ biodb::testThat("show() method works correctly.", test_chebiExShow,
                 biodb=biodb)
 biodb::testThat("We can generate a skeleton for a new extension package.",
                 test_newExtPkgSkeletonGeneration)
-# XXX Turn off `make` test because of failure with `R CMD check`.
-#biodb::testThat("We can run make on a newly generated extension package.",
-#                test_runningMakeOnNewExtPkg)
 biodb::testThat("We can upgrade the files of an extension package.",
                 test_upgradeExtPkg)
 biodb::testThat("We can use '.' (current directory) for the package path.",
