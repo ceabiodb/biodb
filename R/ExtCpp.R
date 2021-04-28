b5f3fe97864b5162067302d73f6ad3c9622b3a64
Pierrick Roger <pk.roger@icloud.com>
2021-04-28 15:57:27 +0200

    Add missing files for running C++ tests.

--- R/ExtCpp.R
+++ R/ExtCpp.R
@@ -37,10 +37,25 @@ initialize=function(...) {
 
 private=list(
 doGenerate=function(overwrite=FALSE, fail=TRUE) {
+    
+    # Copy all C++ files
     templates <- system.file('templates', package='biodb')
     for (f in Sys.glob(paste0(templates, '/*.cpp')))
         private$createGenerator(ExtFileGenerator, template=basename(f),
                                 folder='src', filename=basename(f)
                                 )$generate(overwrite=overwrite, fail=fail)
+    
+    # Add R file that declares run_testthat_tests()
+    private$createGenerator(ExtFileGenerator,
+                            template='catch-routine-registration.R',
+                            folder='R', filename='catch-routine-registration.R'
+                            )$generate(overwrite=overwrite, fail=fail)
+    
+    # Add R file that runs C++ tests from testthat
+    testthatFolder <- c('tests', 'testthat')
+    private$createGenerator(ExtFileGenerator, template='test-cpp.R',
+                            folder=testthatFolder,
+                            filename='test-cpp.R'
+                            )$generate(overwrite=overwrite, fail=fail)
 }
 ))
---
+++ inst/templates/test-cpp.R
@@ -0,0 +1,2 @@
+# Run C++ tests file for biodb extensions packages, version 1.0.0
+run_cpp_tests("{{pkgName}}")
