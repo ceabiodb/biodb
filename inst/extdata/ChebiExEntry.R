a56400dacdf4d9a4f465cbcfb6004cd9dd300632
Pierrick Roger <pk.roger@icloud.com>
2021-05-03 18:52:29 +0200

    Update new_connector vignette with latest changes.

--- inst/extdata/ChebiExEntry.R
+++ inst/extdata/ChebiExEntry.R
@@ -1,3 +1,24 @@
 ChebiExEntry <- methods::setRefClass("ChebiExEntry",
-    contains="BiodbXmlEntry"
-)
+    contains="BiodbXmlEntry",
+
+methods=list(
+
+initialize=function(...) {
+    callSuper(...)
+}
+
+,.isContentCorrect=function(content) {
+ 
+    correct <- callSuper(content)
+    
+    # You can do some more checks of the content here.
+    
+    return(correct)
+}
+
+,.parseFieldsStep2=function(parsed.content) {
+    
+    # TODO Implement your custom parsing processing here.
+}
+
+))
